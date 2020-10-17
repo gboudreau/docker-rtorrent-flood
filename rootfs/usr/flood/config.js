2444b5590faed4af35287accc657069f67ec4004
Guillaume Boudreau <guillaume@pommepause.com>
2020-10-17 12:20:59 -0400

    Updated config

--- rootfs/usr/flood/config.js
+++ rootfs/usr/flood/config.js
@@ -1,25 +1,83 @@
+// This is the configuration file for Flood, a React-based frontend for the
+// rtorrent BitTorrent client.
+// Copy this file to ./config.js and make changes below.
+// config.js must exist before running `npm run build`.
+
 const CONFIG = {
-  baseURI: process.env.WEBROOT || '/',
+  // This URI will prefix all of Flood's HTTP requests. You _must_ have a web
+  // server, like nginx, configured to forward these requests to the Flood
+  // web server.
+  // For example, if you intend to serve from http://example.com/flood, set this to
+  // '/flood' and configure your web server to pass _all_ requests from `/flood` to
+  // the `/flood` of Flood's web server.
+  // See https://github.com/Flood-UI/flood/wiki/Using-Flood-behind-a-reverse-proxy
+  baseURI: '/',
+  // Flood uses a local nedb database to keep track of users, torrents,
+  // and activity. The database is regularly purged to remove outdated data.
+  // This value dictates how old data is, in milliseconds, before being purged.
   dbCleanInterval: 1000 * 60 * 60,
+  // Where to store the local nedb database.
   dbPath: '/flood-db/',
-  floodServerPort: 3000,
-  maxHistoryStates: 30,
-  pollInterval: 1000 * 5,
-  secret: process.env.FLOOD_SECRET || 'secret',
-  scgi: {
+  // Where to store Flood's temporary files
+  tempPath: '/tmp/',
+  // If this is true, there will be no users and no attempt to
+  // authenticate or password-protect any page. In that case,
+  // instead of per-user config, the following configUser settings
+  // will be used.
+  disableUsersAndAuth: true,
+  // Settings for the no-user configuration.
+  configUser: {
+    // How to connect to rTorrent. If this is true, it will connect
+    // to the unix socket defined in socketPath.
+    // Otherwise, it will connect via SCGI with the host and port.
+    socket: true,
+    // Path to the rTorrent unix socket.
+    socketPath: '/tmp/rtorrent.sock',
+    // SCGI host and port to connect to rTorrent.
     host: 'localhost',
     port: 5000,
-    socket: true,
-    socketPath: '/tmp/rtorrent.sock'
   },
+  // The host that Flood should listen for web connections on.
+  // If you want to connect to Flood from hosts other that the one it is running
+  // on, you should change this value.
+  // To listen on all interfaces, change to `floodServerHost: '0.0.0.0'`..
+  floodServerHost: '0.0.0.0',
+  // The port that Flood should listen for web connections on.
+  floodServerPort: 3000,
+  // Used for development. See the "Local Development" section of README.md
+  // for detail.
+  floodServerProxy: 'http://127.0.0.1:3000',
+  // Flood keeps a history of torrent download and upload speeds.
+  // This value dictates the number of individual records per period to keep.
+  maxHistoryStates: 30,
+  // How often (in milliseconds) Flood will request the torrent list from.
+  torrentClientPollInterval: 1000 * 2,
+  // A unique secret for signing messages with JWT (see https://jwt.io).
+  // Change this to something unique and hard to guess.
+  // You can use 'uuidgen' or 'cat /proc/sys/kernel/random/uuid' or 'uuidgenerator.net'.
+  // eslint-disable-next-line no-undef
+  secret: process.env.FLOOD_SECRET || 'secret',
+  // Configuration for SSL, if using SSL with the Flood service directly.
+  ssl: false,
+  sslKey: '/absolute/path/to/key/',
+  sslCert: '/absolute/path/to/certificate/',
+  // disk space service checks disk space of mounted partitions
   diskUsageService: {
     // assign desired mounts to include. Refer to "Mounted on" column of `df -P`
     watchMountPoints: [
-      "/data/torrents",
-      "/data/torrents2",
-      "/data/torrents6"
+      "/data/torrents5",
+      "/mnt/hdd1",
+      "/mnt/hdd2",
+      "/mnt/hdd4",
+      "/mnt/hdd5",
+      "/mnt/hdd6",
+      "/mnt/hdd7",
+      "/mnt/hdd8",
+      "/mnt/hdd9"
     ]
-  }
+  },
+  // Allowed paths for file operations
+  // allowedPaths: [],
 };
-
+// Do not remove the below line.
 module.exports = CONFIG;
